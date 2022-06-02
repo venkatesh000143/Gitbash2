d6ef2b12a5c9d40517036b7d0d6fe0ee88aba3de
Mr. Venky <venkatesh000143>
2022-06-02 11:20:55 +0530

    venky4 first commit

---
+++ src/test/java/com/jenkins/Test/GetTicketsTest.java
@@ -0,0 +1,11 @@
+  package com.jenkins.Test;
+
+import org.testng.annotations.Test;
+
+public class GetTicketsTest {
+	@Test
+public void gitBashPractice()
+{
+	System.out.println("gitbash run");
+}
+}
